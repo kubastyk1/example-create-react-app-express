870acf0f6bc0e4ab208313675d47e4c06324177e
Jakub Stawiszyński <kubastyk@wp.pl>
2018-07-04 19:11:50 +0200

    Added form field for adding elements to list

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,16 @@ import './App.css';
 import SortableComponent from './SortableComponent.js';
 
 class App extends Component {
-  state = {
-    response: []
-  };
+  constructor () {
+    super();
+    this.state = {
+      response: [],
+      original: '',
+      translated: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   componentDidMount() {
     this.callApi()
@@ -23,6 +30,22 @@ class App extends Component {
     return body;
   };
 
+  handleSubmit(event) {
+    event.preventDefault();
+    let response = this.state.response;
+    let newValue = {
+      original: this.state.original,
+      translated: this.state.translated
+    }
+    response.push(newValue);
+    this.setState({ response: response })
+  };
+
+  handleChange (event) {
+
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
     return (
       <div className="App">
@@ -30,10 +53,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Original:
+            <input type="text" onChange={this.handleChange} name="original" required/>
+          </label>
+          <label>
+            Translated:
+            <input type="text" onChange={this.handleChange} name="translated" required/>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
         <div className="SampleContainer">
-          {
-            <SortableComponent items={this.state.response}/>
-          }
+          <SortableComponent items={this.state.response}/>
         </div>
       </div>
     );
